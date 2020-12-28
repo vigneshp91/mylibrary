86e574c93c5c0a4ae428e2764c5c9864d8985a9b
Vignesh <vigneshkvs.p@gmail.com>
2020-12-28 21:31:50 +0530

    class changes

--- app/src/main/java/com/example/mylib/MainActivity.kt
+++ app/src/main/java/com/example/mylib/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.mylib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mylibrary.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToasterMessage.showMessage(this,"Hello library")
     }
 }
