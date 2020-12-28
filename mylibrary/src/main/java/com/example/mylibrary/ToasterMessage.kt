a764d03ab78ead4e2eb205975c2289f688be60f4
Vignesh <vigneshkvs.p@gmail.com>
2020-12-28 21:25:37 +0530

    class changes

--- mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.kt
+++ mylibrary/src/main/java/com/example/mylibrary/ToasterMessage.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.widget.Toast
 
 public class ToasterMessage {
-    fun showMessage(ctx: Context, message:String?){
-        if(!message.isNullOrEmpty())
-            Toast.makeText(ctx,message,Toast.LENGTH_LONG).show()
+    companion object{
+        fun showMessage(ctx: Context, message:String?){
+            if(!message.isNullOrEmpty())
+                Toast.makeText(ctx,message,Toast.LENGTH_LONG).show()
+        }
     }
+
 }
